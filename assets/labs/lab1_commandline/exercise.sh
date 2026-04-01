3601b4b4ade6faddeba9876185bd0cb393ba7140
Nowcarpediem <94575562+Nowcarpediem@users.noreply.github.com>
2026-03-31 20:11:41 -0700

    clear

--- assets/labs/lab1_commandline/exercise.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p ~/cse29/lab1 cse29/lab1/Music cse29/lab1/Books
-
-touch ~/cse29/lab1/Music/"Auld Lang Syne.mp3"
-touch ~/cse29/lab1/Music/"Canon in D.mp3"
-touch ~/cse29/lab1/Books/OSTEP.txt
-touch ~/cse29/lab1/Books/Frankenstein.txt
-touch ~/cse29/lab1/Books/"The Little Prince.txt"
--- docs/labs/index.md
+++
@@ -1,8 +0,0 @@
----
-layout: default
-title: Labs
-nav_order: 8
-has_children: true
----
-
-# Labs
