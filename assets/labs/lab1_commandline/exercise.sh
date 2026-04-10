61e7f9a63f263c25f54e6055c53a9df369bdcf37
Monip1 <elena@ratmotor.com>
2026-04-10 10:41:42 -0700

    made exercise.sh not matter what directory it's run from

--- assets/labs/lab1_commandline/exercise.sh
+++ assets/labs/lab1_commandline/exercise.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p ~/cse29/lab1 cse29/lab1/Music cse29/lab1/Books
+mkdir -p ~/cse29/lab1 ~/cse29/lab1/Music ~/cse29/lab1/Books
 
 touch ~/cse29/lab1/Music/"Auld Lang Syne.mp3"
 touch ~/cse29/lab1/Music/"Canon in D.mp3"
